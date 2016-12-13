e944f559d243c424c7288cac6227a61c2b9c94d9
Genadi Sokolov <genadis.pub@gmail.com>
2016-12-13 15:17:44 +0200

    deprecated publish upgrade events, implemented basic component tests (covers button and textfield), implemented downgrade elements on detach(), updated README and ISSUE_TEMPLATE, added coverage

---
+++ test/unit/textfield.spec.js
@@ -0,0 +1,24 @@
+import {StageComponent} from 'aurelia-testing';
+import {bootstrap} from 'aurelia-bootstrapper';
+
+describe('ButtonAttribute', () => {
+  let component;
+
+  beforeEach(() => {
+    component = StageComponent
+      .withResources('mdl')
+      .inView('<button mdl="button">Button</button>')
+      .boundTo({});
+  });
+
+  it('should upgrade the button with MaterialButton', done => {
+    component.create(bootstrap).then(() => {
+      expect(component.element.MaterialButton).toBeDefined();
+      done();
+    }).catch(e => console.log(e.toString()));
+  });
+
+  afterEach(() => {
+    component.dispose();
+  });
+});
